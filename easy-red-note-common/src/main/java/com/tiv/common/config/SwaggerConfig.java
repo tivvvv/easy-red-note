c741306552f46fcff8d06a2ccc5cf577572e2270
tivvvv <18612553935@163.com>
2025-06-07 00:24:06 +0800

    feat: knife4j替换为springfox.

--- easy-red-note-common/src/main/java/com/tiv/common/config/SwaggerConfig.java
+++ easy-red-note-common/src/main/java/com/tiv/common/config/SwaggerConfig.java
@@ -1,32 +1,41 @@
 package com.tiv.common.config;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
- * Knife4j文档配置
+ * Swagger配置类
  */
-@Configuration
-@EnableSwagger2WebMvc
-public class Knife4jConfig {
+@Component
+@Data
+@EnableOpenApi
+@EnableWebMvc
+@Slf4j
+public class SwaggerConfig {
 
     @Bean
-    public Docket userApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
+    public Docket ApiDoc() {
+        return new Docket(DocumentationType.OAS_30)
                 .groupName("用户端服务接口文档")
+                .pathMapping("/")
+                .enable(true)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.tiv.user.controller"))
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.tiv"))
+                .paths(PathSelectors.ant("/api/**"))
                 .build();
+
     }
 
     private ApiInfo apiInfo() {
